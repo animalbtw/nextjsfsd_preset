8cd46556bd35cc0c81deed6583fbf616f066ce88
unknown <animalbtw@gmail.com>
2025-03-20 07:57:37 +0300

    airbnb eslint added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import perfectionistConfig from 'eslint-plugin-perfectionist/configs/recommended-natural';
+import regexpPlugin from 'eslint-plugin-regexp';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...compat.extends("airbnb"),
+    ...compat.extends("airbnb-typescript"),
+    ...tseslint.configs.recommendedTypeChecked,
+    perfectionistConfig,
+    regexpPlugin.configs['flat/recommended'],
+    {
+      plugins: {
+        'simple-import-sort': simpleImportSort,
+        unicorn,
+      },
+      languageOptions: {
+        parserOptions: {
+          project: true,
+          tsconfigRootDir: __dirname,
+        },
+      },
+      rules: {
+      }
+    },
+    {
+      ignores: [
+        'src/client/vite-env.d.ts',
+      ]
+    }
+);
